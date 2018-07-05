042f0d43af0da986f5ecfca477ff2f1f5d895081
noribang <hoonk9387@gmail.com>
2018-07-05 12:01:29 -0500

    Added notes to .txt. Added alter table statements to .sql file.

--- users.sql
+++ users.sql
@@ -3,3 +3,37 @@ id serial UNIQUE NOT NULL,
 username char(25),
 enabled boolean DEFAULT TRUE
 );
+
+-- Rename Table.
+ALTER TABLE users
+RENAME TO all_users;
+
+-- Rename Column.
+ALTER TABLE all_users
+RENAME COLUMN username TO full_name;
+
+-- Change Data Type.
+ALTER TABLE all_users
+ALTER COLUMN full_name TYPE varchar(50)
+
+-- Change Constraints.
+ALTER TABLE all_users
+ALTER COLUMN full_name SET NOT NULL;
+
+-- Remove Constraints.
+ALTER TABLE all_users
+ALTER COLUMN id DROP DEFAULT;
+
+-- Add Column.
+ALTER TABLE all_users
+ADD COLUMN last_login timestamp NOT NULL DEFAULT NOW();
+
+-- Remove Column.
+ALTER TABLE all_users
+DROP COLUMN enabled;
+
+-- Remove Table.
+DROP TABLE all_users;
+
+
+
